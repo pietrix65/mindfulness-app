b92b641575937daae6c0ac8a4aad949b1cd88268
Pedro Garcia-Die <pedro.garciadie@gmail.com>
2020-07-22 12:18:47 +0200

    video 04 11

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
 class Directory extends React.Component {
    
     constructor() {
@@ -7,7 +11,61 @@ class Directory extends React.Component {
         super();
 
         this.state = {
-            sections: 
+            sections: [{
+                titulo: 'Evitar juicios',
+                imgageUrl: 'https://i.ibb.co/09vvr7W/Webp-net-resizeimage-7.jpg',
+                id: 1
+            },
+            
+            {
+                titulo: 'Paciencia',
+                imgageUrl: 'https://i.ibb.co/PGzkBsW/Webp-net-resizeimage.jpg',
+                id: 2
+            },
+            {
+                titulo: 'Mente Principiante',
+                imgageUrl: 'https://i.ibb.co/hfVPyQQ/Webp-net-resizeimage-5.jpg',
+                id: 3
+            },
+            {
+                titulo: 'Confianza',
+                imgageUrl: 'https://i.ibb.co/GsRfnN8/Webp-net-resizeimage-2.jpg',
+                id: 4
+            },
+            {
+                titulo: 'Evitar Forzar',
+                imgageUrl: 'https://i.ibb.co/McyfyVR/Webp-net-resizeimage-4.jpg',
+                id: 5
+            },
+            {
+                titulo: 'Aceptación',
+                imgageUrl: 'https://i.ibb.co/Sd2JzfX/Webp-net-resizeimage-6.jpg',
+                id: 6
+            },
+            
+            {
+                titulo: 'Dejar Ir',
+                imgageUrl: 'https://i.ibb.co/9cQ5SQP/Webp-net-resizeimage-3.jpg',
+                size: 'large',
+                id: 7
+            }
+            
+            
+            ]
         }
     }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({titulo,imgageUrl,id , size}) => (
+                        <MenuItem key={id} title={titulo}  imageUrl={imgageUrl} size={size} />
+                    ))
+                }
+            </div>
+        )
+    }
 }
+
+export default Directory;
